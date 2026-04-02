caddcbd784f17494d366f40fa4d83b2538f435bf
Nikita Karanpuria <nikita.vhits@gmail.com>
2026-04-02 12:09:43 +0530

    migrate remove

---
+++ backend/prisma/migrations/20260402063920_removeovertimerequest/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "overtime_requests" DROP CONSTRAINT "overtime_requests_clientId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "overtime_requests" DROP CONSTRAINT "overtime_requests_employeeId_fkey";
